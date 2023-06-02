1b1a4129c516dd5f120421c43053815b012bd3ee
Tarnishtoken <134907388+Tarnishtoken@users.noreply.github.com>
2023-06-01 22:13:22 -0500

    Add files via upload

---
+++ components/InstructionsComponent.jsx
@@ -0,0 +1,84 @@
+import styles from "../styles/InstructionsComponent.module.css";
+import Router, { useRouter } from "next/router";
+export default function InstructionsComponent() {
+	const router = useRouter();
+	return (
+		<div className={styles.container}>
+			<header className={styles.header_container}>
+				<h1>
+					Tarnish<span>Token</span>
+				</h1>
+				<p>
+					
+					
+					{" "}
+					<span></span>
+					
+					
+				</p>
+			</header>
+
+			<div className={styles.buttons_container}>
+				<a
+					target={"_blank"}
+					href={"https://metamask.io/"}
+				>
+					<div className={styles.button}>
+						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
+						<p>create a wallet</p>
+					</div>
+				</a>
+				<a
+					target={"_blank"}
+					href={"https://ebutemetaverse.com/how-to-buy-matic-on-metamask/"}
+				>
+					<div className={styles.button}>
+						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
+						<p>buy some matic</p>
+					</div>
+				</a>
+				<a
+					target={"_blank"}
+					href={"https://app.uniswap.org/#/tokens/polygon/0x97780be6224d62b76d20493da7056ec86eeae6f6"}
+				>
+					<div className={styles.button}>
+						<img
+							src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
+							width={"20px"}
+							height={"20px"}
+						/>
+						<p>go to uniswap</p>
+					</div>
+				</a>
+			</div>
+			<div className={styles.footer}>
+				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+					<img
+						id="badge-button"
+						style={{ width: "240px", height: "53px" }}
+						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
+						alt="Alchemy Supercharged"
+					/>
+				</a>
+				<div className={styles.icons_container}>
+					<div>
+						<a
+							href="https://github.com/Tarnishtoken"
+							target={"_blank"}
+						>
+							Github
+						</a>
+					</div>
+					<div>
+						<a
+							href="https://twitter.com/tarnishtoken"
+							target={"_blank"}
+						>
+							Twitter
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
