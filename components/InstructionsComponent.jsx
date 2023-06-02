616617929607fbcbbbdcd8de0d98fe7c53a8ce17
Tarnishtoken <134907388+Tarnishtoken@users.noreply.github.com>
2023-06-01 22:40:53 -0500

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -55,22 +55,7 @@ export default function InstructionsComponent() {
 					</div>
 				</a>
 			</div>
-			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-					<img
-						id="badge-button"
-						style={{ width: "240px", height: "53px" }}
-						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
-						alt="Alchemy Supercharged"
-					/>
-				</a>
-				<div className={styles.icons_container}>
-					<div>
-						<a
-							href="https://github.com/Tarnishtoken"
-							target={"_blank"}
-						>
-							Github
+			
 						</a>
 					</div>
 					<div>
